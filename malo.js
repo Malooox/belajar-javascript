c658c68bb917b393e91d311721dab78d4d4ed3ae
Malooox <akmalgklcbs@gmail.com>
2024-05-09 16:00:59 +0800

    first commit

---
+++ malo.js
@@ -0,0 +1,11 @@
+//belajar if else
+
+const nilai = "50";
+
+if (nilai > 50) {
+    console.log(`Nilai anda ${nilai}. Anda di nyatakan lulus`);
+}
+
+else {
+    console.log(`Nilai anda ${nilai}. Anda di nyatakan tidak lulus`);
+}
---
+++ script.js
@@ -0,0 +1,28 @@
+
+//belajar operator
+// + - * /
+
+const nilai1 = 20;
+const nilai2 = 30;
+
+//menggunakan operator tambah
+//gunakan + untuk menyatukan string dan variabel
+
+console.log("Hasil Penjumlahan : " +(nilai1 + nilai2));
+console.log("Hasil Pengurangan : " +(nilai1 - nilai2)); 
+console.log("Hasil Bagi : " +(nilai1 / nilai2));
+console.log("Hasil kali :" +(nilai1 * nilai2));
+
+let biodata = {
+    nama: "Malo",
+    umur: "25",
+    alamat: "jln Bataraguru",
+
+}
+
+console.log(biodata); // memanggil semua data
+console.log(`-------------`); //memanggil salah satu data
+
+console.log(`Halo nama saya adalah ${biodata.nama}. saya berumur ${biodata.umur}. saya tinggal di alamat ${biodata.alamat}.`)
+
+
